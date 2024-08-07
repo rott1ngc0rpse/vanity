2e2c28c81add37bab8ad6370b710eaf7439ac5ca
Salem <stxrrypaws@proton.me>
2024-08-07 13:02:58 -0400

    commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Vanity, all in one discord bot.
--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -12,7 +12,7 @@ const commandFolders = fs.readdirSync(path.join(__dirname, 'commands'));
 for (const folder of commandFolders) {
   const commandFiles = fs
     .readdirSync(path.join(__dirname, 'commands', folder))
-    .filter(file => file.endsWith('.js'));  // Changed '.ts' to '.js' since we're using JavaScript
+    .filter(file => file.endsWith('.js'));
 
   for (const file of commandFiles) {
     const command = require(`./commands/${folder}/${file}`);
